fac7e873bb5f75b62cd68972db773264cf53f621
Cameron Cooper <coopercc06@gmail.com>
2024-11-17 18:58:06 -0600

    probably done with icmp portion of os_engine

--- src/nmap/os_engine/icmp.rs
+++ src/nmap/os_engine/icmp.rs
@@ -12,9 +12,9 @@ use pnet::{
     transport::{self, icmp_packet_iter, TransportChannelType},
     util::checksum,
 };
-use rand::{thread_rng, Rng};
+use rand::{random, thread_rng, Rng};
 use local_ip_address::{linux::local_ip};
-
+use byteorder::{BigEndian, BE};
 
 const windowSizes: [u16; 13] = [ 1, 63, 4, 4, 16, 512, 3, 128, 256, 1024, 31337, 32768, 65536 ];
 /*
@@ -41,6 +41,11 @@ enum IcmpError {
     Generic
 }
 
+struct IeProbes {
+    p1: Option<Vec<u8>>,
+    p2: Option<Vec<u8>>,
+    icmp_id: u16
+}
 
 const DF: u8 = 0b010;
 const MF: u8 = 0b001;
@@ -50,67 +55,147 @@ fn get_local_ip_addr() -> Ipv4Addr {
     Ipv4Addr::from_str(&str).unwrap()
 }
 
-/** MAKE SURE THE OK() RESULT IS USED AND MOVED TO EchoReplyPacket */
-fn send_packet1(ip_addr: Ipv4Addr) -> Result<Vec<u8>, String> {
-    let protocol = TransportChannelType::Layer3(IpNextHeaderProtocols::Icmp);
-    // TODO move this to a match statement and syslog it. this is a daemon, not a user process
-    let (mut tx, mut rx) = transport::transport_channel(1024, protocol)
-        .expect("Failed to create transport channel");
+impl IeProbes {
 
-    let mut buf = vec![0u8; 64];
-    
-    let id = thread_rng().gen();
-    // single packet for now
-    let mut packet = MutableEchoRequestPacket::new(&mut buf)
-        .expect("Failed to create ICMP packet");
-    packet.set_icmp_type(EchoRequest);
-    packet.set_sequence_number(295);
-    packet.set_icmp_code(IcmpCode::new(9));
-    packet.set_identifier(id);
-    // payload of 120 bytes of 0x0
-    packet.set_payload(&[0u8; 120]);
-
-    let ip_header_len = 20;
-    let icmp_len = packet.packet().len();
-    let total_len = ip_header_len + icmp_len;
-
-    let mut raw_packet = vec![0u8; total_len];
-    let splices = &mut raw_packet.split_at_mut(ip_header_len);
-    let mut ip_packet = MutableIpv4Packet::new(&mut splices.0)
-        .expect("Failed to create IP packet");
-    ip_packet.set_version(4);
-    ip_packet.set_header_length(ip_header_len as u8/4); // ip header length measured in words. Says
-                                                        // u8 but it really is u4
-                                                    
-    ip_packet.set_total_length(total_len as u16);
-    ip_packet.set_identification(id);
-    ip_packet.set_flags(DF);
-    ip_packet.set_ttl(64);
-    ip_packet.set_next_level_protocol(Icmp);
-    ip_packet.set_source(get_local_ip_addr());
-    ip_packet.set_destination(ip_addr);
-    
-    
-    let ip_payload = &mut splices.1;
-    ip_payload.copy_from_slice(packet.packet());
-
-    let ip_checksum = checksum(&ip_packet.packet(), 1);
-    ip_packet.set_checksum(ip_checksum);
-
-    tx.send_to(ip_packet, IpAddr::V4(ip_addr)).expect("Failed to send packet");
-    
-    let mut iter = icmp_packet_iter(&mut rx);
-    let (reply, addr) = iter.next_with_timeout(Duration::from_millis(500)).expect("Failed to get reply packet").expect("Failed to get reply packet");
-
-
-    if addr == IpAddr::from(ip_addr) {
-        let mut v: Vec<u8> = Vec::new();
-        for i in reply.packet().iter() {
-            v.push(*i);
+    /** IeProbes must follow immediately after TCP probes in order to work */
+    fn new() -> IeProbes {
+        let mut rng = thread_rng();
+        let x: f64 = rng.gen();
+        let y = (u16::max_value() - 1) as f64 * x;
+        IeProbes {
+            p1: None,
+            p2: None,
+            icmp_id: y.floor() as u16
         }
-        return Ok(v)
     }
-    return Err("Failed to get reply".to_string())
+    /** MAKE SURE THE OK() RESULT IS USED AND MOVED TO EchoReplyPacket */
+    fn send_packet1(&self, ip_addr: Ipv4Addr) -> Result<Vec<u8>, String> {
+        let protocol = TransportChannelType::Layer3(IpNextHeaderProtocols::Icmp);
+        // TODO move this to a match statement and syslog it. this is a daemon, not a user process
+        let (mut tx, mut rx) = transport::transport_channel(1024, protocol)
+            .expect("Failed to create transport channel");
+    
+        let mut buf = vec![0u8; 64];
+        
+        let id = thread_rng().gen();
+        // single packet for now
+        let mut packet = MutableEchoRequestPacket::new(&mut buf)
+            .expect("Failed to create ICMP packet");
+        packet.set_icmp_type(EchoRequest);
+        packet.set_sequence_number(295);
+        packet.set_icmp_code(IcmpCode::new(9));
+        packet.set_identifier(self.icmp_id);
+        // payload of 120 bytes of 0x0
+        packet.set_payload(&[0u8; 120]);
+    
+        let ip_header_len = 20;
+        let icmp_len = packet.packet().len();
+        let total_len = ip_header_len + icmp_len;
+    
+        let mut raw_packet = vec![0u8; total_len];
+        let splices = &mut raw_packet.split_at_mut(ip_header_len);
+        let mut ip_packet = MutableIpv4Packet::new(&mut splices.0)
+            .expect("Failed to create IP packet");
+        ip_packet.set_version(4);
+        ip_packet.set_header_length(ip_header_len as u8/4); // ip header length measured in words. Says
+                                                            // u8 but it really is u4
+                                                        
+        ip_packet.set_total_length(total_len as u16);
+        ip_packet.set_identification(id);
+        ip_packet.set_flags(DF);
+        ip_packet.set_ttl(64);
+        ip_packet.set_next_level_protocol(Icmp);
+        ip_packet.set_source(get_local_ip_addr());
+        ip_packet.set_destination(ip_addr);
+        
+        
+        let ip_payload = &mut splices.1;
+        ip_payload.copy_from_slice(packet.packet());
+    
+        let ip_checksum = checksum(&ip_packet.packet(), 1);
+        ip_packet.set_checksum(ip_checksum);
+    
+        tx.send_to(ip_packet, IpAddr::V4(ip_addr)).expect("Failed to send packet");
+        
+        let mut iter = icmp_packet_iter(&mut rx);
+        let (reply, addr) = iter.next_with_timeout(Duration::from_millis(500)).expect("Failed to get reply packet").expect("Failed to get reply packet");
+    
+    
+        if addr == IpAddr::from(ip_addr) {
+            let mut v: Vec<u8> = Vec::new();
+            for i in reply.packet().iter() {
+                v.push(*i);
+            }
+            return Ok(v)
+        }
+        return Err("Failed to get reply".to_string())
+    }
+
+    fn send_packet2(&self, ip_addr: Ipv4Addr) -> Result<Vec<u8>, String> {
+        let protocol = TransportChannelType::Layer3(IpNextHeaderProtocols::Icmp);
+        // TODO move this to a match statement and syslog it. this is a daemon, not a user process
+        let (mut tx, mut rx) = transport::transport_channel(1024, protocol)
+            .expect("Failed to create transport channel");
+    
+        let mut buf = vec![0u8; 64];
+        
+        // single packet for now
+        let mut packet = MutableEchoRequestPacket::new(&mut buf)
+            .expect("Failed to create ICMP packet");
+        packet.set_icmp_type(EchoRequest);
+        packet.set_sequence_number(296);
+        packet.set_icmp_code(IcmpCode::new(0));
+        packet.set_identifier(self.icmp_id+1);
+        // payload of 120 bytes of 0x0
+        packet.set_payload(&[0u8; 150]);
+    
+        let ip_header_len = 20;
+        let icmp_len = packet.packet().len();
+        let total_len = ip_header_len + icmp_len;
+    
+        let mut raw_packet = vec![0u8; total_len];
+        let splices = &mut raw_packet.split_at_mut(ip_header_len);
+        let mut ip_packet = MutableIpv4Packet::new(&mut splices.0)
+            .expect("Failed to create IP packet");
+        ip_packet.set_version(4);
+        ip_packet.set_header_length(ip_header_len as u8/4); // ip header length measured in words. Says
+                                                            // u8 but it really is u4
+        let tos = 0x04;
+        ip_packet.set_total_length(total_len as u16);
+        ip_packet.set_dscp(tos >> 2);
+        ip_packet.set_ecn(0x00);
+        ip_packet.set_identification(self.icmp_id+1);
+        ip_packet.set_flags(DF);
+        ip_packet.set_ttl(64);
+        ip_packet.set_next_level_protocol(Icmp);
+        ip_packet.set_source(get_local_ip_addr());
+        ip_packet.set_destination(ip_addr);
+        
+        
+        let ip_payload = &mut splices.1;
+        ip_payload.copy_from_slice(packet.packet());
+    
+        let ip_checksum = checksum(&ip_packet.packet(), 1);
+        ip_packet.set_checksum(ip_checksum);
+    
+        tx.send_to(ip_packet, IpAddr::V4(ip_addr)).expect("Failed to send packet");
+        
+        let mut iter = icmp_packet_iter(&mut rx);
+        let (reply, addr) = iter.next_with_timeout(Duration::from_millis(500)).expect("Failed to get reply packet").expect("Failed to get reply packet");
+    
+    
+        if addr == IpAddr::from(ip_addr) {
+            let mut v: Vec<u8> = Vec::new();
+            for i in reply.packet().iter() {
+                v.push(*i);
+            }
+            return Ok(v)
+        }
+        return Err("Failed to get reply".to_string())
+    }
+
+    fn send_probes(&mut self, addr: Ipv4Addr) {
+        self.p1 = Some(self.send_packet1(addr).expect("icmp probe1 failed"));
+        self.p2 = Some(self.send_packet2(addr).expect("icmp probe2 failed"))
+    }
 }
-
-
